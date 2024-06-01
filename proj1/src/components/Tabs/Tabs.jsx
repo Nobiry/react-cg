f86846695df778c38d0f01c68c6ed9ba01f6b9b0
Nobiry <nobiry48@gmail.com>
2024-06-01 14:09:50 +0300

    react-cg start proj2

--- proj1/src/components/Tabs/Tabs.jsx
+++ proj1/src/components/Tabs/Tabs.jsx
@@ -9,3 +9,29 @@ export default function Tabs({ children, buttons, BtnContainer = "menu" }) {
         </>
     ); 
 }
+export default function Button({ children, mode = "filled", Icon, ...props }) {
+    let modeClass;
+    switch (mode) {
+        case 'outline':
+            modeClass = "outline-button";
+            break;
+        case 'text':
+            modeClass = "text-button";
+            break;
+        default: 
+            modeClass = "filled-button";
+            break;
+    }
+    
+    return (
+        <button {...props} className={`button ${modeClass} ${Icon ? 'button-icon' : ''}`}>
+            ${Icon ? <span>{Icon}</span> : ''}
+            <span>{ children }</span>
+        </button>    
+    );
+ // Todo: Build this component!
+ 
+ // !!! Important: 
+ // Wrap the icon with a <span className="button-icon"> to achieve the target look
+ // Also wrap the children prop with a <span>
+}
